e25004ddb1987683f89b8aaa63a63421c7303161
oalru <OsamaAlruthaya@hotmail.com>
2020-07-10 08:57:27 +0300

    fixed some Bugs

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import {Container} from 'react-bootstrap'
-// import {twitter, facebook, linkedin, github} from './Images';
 
 const About = (props) => {
     return(
         <Container>
             <h3>To Reach Me, Please Choose one of the following Channels:</h3>
-            <ul>
-                {/* <li><a href="https://www.twitter.com"><img src={twitter}/></a></li>
-                <li><a href="https://www.facebook.com"><img src={facebook}/></a></li>
-                <li><a href="https://www.linkedin.com"><img src={linkedin}/></a></li>
-                <li><a href="https://www.github.com"><img src={github}/></a></li> */}
-            </ul>
+            
+                <div><a href="https://www.twitter.com"><img height="50px" src="https://image.flaticon.com/icons/svg/174/174876.svg"/></a></div>
+                <div><a href="https://www.facebook.com"><img height="50px" src="https://image.flaticon.com/icons/svg/174/174848.svg"/></a></div>
+                <div><a href="https://www.linkedin.com"><img height="50px" src="https://image.flaticon.com/icons/svg/145/145807.svg"/></a></div>
+                <div><a href="https://www.github.com"><img height="50px" src="https://image.flaticon.com/icons/svg/2111/2111425.svg"/></a></div> 
+            
         </Container>
     );
 }
